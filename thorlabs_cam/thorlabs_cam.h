45a9f2657d56ef64d0fac62eefcd8afa442053bb
Clemens Schäfermeier <clemens@fh-muenster.de>
2019-08-21 10:41:49 +0200

    Updates to Makefile

--- thorlabs_cam/thorlabs_cam.h
+++ thorlabs_cam/thorlabs_cam.h
@@ -25,14 +25,14 @@ private:
     BOARDINFO b_info;
 
     int err,
-        memID; /**< grabber memory - buffer ID */
+        memID; /**< Grabber memory - buffer ID */
 
-    char *im_mem; /**< pointer to buffer */
+    char *im_mem; /**< Pointer to buffer */
 
     uint32_t im_width,
-             im_max_width, /**< initial or maximal width */
+             im_max_width, /**< Initial or maximal width */
              im_height,
-             im_max_height, /**< initial or maximal height */
+             im_max_height, /**< Initial or maximal height */
              pix_clock,
              im_min_width, /**< AOI minimum width */
              im_min_height, /**< AOI minimum height */
@@ -42,20 +42,20 @@ private:
              im_aoi_height, /**< full AOI height */
              im_aoi_width_start, /**< start position (in pixel) of the AOI */
              im_aoi_height_start, /**< start position (in pixel) of the AOI */
-             sensor_aa_width, /**< exact active width of the sensor in um */
-             sensor_aa_height; /**< exact active height of the sensor in um */
+             sensor_aa_width, /**< Exact active width of the sensor in um */
+             sensor_aa_height; /**< Exact active height of the sensor in um */
 
-    uint16_t pix_size, /**< pixel size encoded in an integer */
-             bits_p_pix, /**< the total number of bits occupied at one pixel.
-                              not all may be used,
+    uint16_t pix_size, /**< Pixel size encoded in an integer */
+             bits_p_pix, /**< The total number of bits occupied at one pixel.
+                              Not all may be used,
                               even when the value is e.g. 32 */
-             color_mod, /**< control the colour mode of the images */
+             color_mod, /**< Control the colour mode of the images */
              color_mod_test,
              color_mod_init;
 
-    double dpix_size, /**< pixel size in um */
+    double dpix_size, /**< Pixel size in um */
            fps,
-           exp_time, /**< exposure time in ms */
+           exp_time, /**< Exposure time in ms */
            exp_time_min,
            exp_time_max,
            exp_time_inc;
@@ -72,7 +72,7 @@ private:
          supp_fine_inc_exp_time;
 
     cv::Mat im_p,
-        infotbar_win_mat;
+            infotbar_win_mat;
 
     std::string infotbar_win_name,
                 sensorname;
@@ -99,7 +99,9 @@ private:
     void TrackbarHandlerStartAOIWidth(int i);
     static void cast_static_SetTrackbarHandlerStartAOIWidth(int i, void *ptr);
     void create_TrackbarStartAOIWidth(void);
-    void set_MouseEvent(const int event, const int x, const int y,
+    void set_MouseEvent(const int event,
+                        const int x,
+                        const int y,
                         const int flags);
 
 public:
@@ -113,9 +115,10 @@ public:
     void alloc_ImageMem(void);
     void set_ImageMem(void);
     void set_ImageSize(void);
-    void inquire_ImageMem(int *res_pt nx, int *res_pt ny,
-                int *res_pt bits_p_pix,
-                int *res_pt pixel_bit_pitch);
+    void inquire_ImageMem(int *res_pt nx,
+                          int *res_pt ny,
+                          int *res_pt bits_p_pix,
+                          int *res_pt pixel_bit_pitch);
     bool caught_Error(void);
     void show_CameraTrackbars(void);
     double get_PixelPitch(void);
@@ -123,18 +126,19 @@ public:
     bool get_Image(cv::Mat &img);
     bool get_Image(void);
     static void cast_static_set_MouseEvent(const int event,
-                                            const int x, const int y,
-                                            const int flags,
-                                            void *u);
+                                           const int x,
+                                           const int y,
+                                           const int flags,
+                                           void *u);
     int get_Width(void);
     int get_Height(void);
     void create_TrackbarExposure(void);
     void draw_CameraInfo(void);
     /* The following functions are accessed by wxWidgets. */
     void get_ExposureTimesAtomic(double *res_pt time,
-                            double *res_pt min_time,
-                            double *res_pt max_time,
-                            double *res_pt inc_time);
+                                 double *res_pt min_time,
+                                 double *res_pt max_time,
+                                 double *res_pt inc_time);
     void set_ExposureTimeAtomic(double time);
     void exchange_ExposureTimeAtomic(void);
     std::string get_CameraInfo(void);
